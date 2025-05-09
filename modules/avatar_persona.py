6bea9d6939e8b5c0cd5ce9be9d1027117b9e9098
Sianlk <96393513+Sianlk@users.noreply.github.com>
2025-05-09 03:18:35 +0100

    Create avatar_persona.py

---
+++ modules/avatar_persona.py
@@ -0,0 +1,4 @@
+def update_avatar(user_id, body_data, clothing_style):
+    print(f"Updating avatar for {user_id} with style {clothing_style}")
+    # TODO: Connect to 3D engine, adjust visuals
+    return "Avatar updated"
