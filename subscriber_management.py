9f8a0a680a8b1929b14151c65b6e14d9dbad1595
Sianlk <96393513+Sianlk@users.noreply.github.com>
2025-05-09 03:41:53 +0100

    Create subscriber_management.py

---
+++ subscriber_management.py
@@ -0,0 +1,4 @@
+def manage_subscribers(action, user_id):
+    print(f"Performing {action} for subscriber {user_id}")
+    # TODO: Add/remove subscribers, handle billing, send newsletters
+    return "Subscriber updated"
