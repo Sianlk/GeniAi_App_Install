81eebe3cecbb029851e831435bd3749f8894ed32
Sianlk <96393513+Sianlk@users.noreply.github.com>
2025-05-09 03:03:48 +0100

    Create voip_system.py

---
+++ modules/voip_system.py
@@ -0,0 +1,6 @@
+import os
+
+def initiate_voip_call(user_id, recipient_id):
+    print(f"Starting VOIP call between {user_id} and {recipient_id}")
+    # TODO: Integrate VOIP backend (Twilio, Agora)
+    return True
