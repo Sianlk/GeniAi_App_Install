e06d25560f0ebe0b55f27b806b4edd4ebc41f27d
Sianlk <96393513+Sianlk@users.noreply.github.com>
2025-05-09 03:23:42 +0100

    Create ecommerce.py

---
+++ modules/ecommerce.py
@@ -0,0 +1,4 @@
+def add_to_cart(user_id, product_id, quantity):
+    print(f"User {user_id} added {quantity} of {product_id} to cart")
+    # TODO: Connect to inventory, pricing, and checkout systems
+    return "Added to cart"
